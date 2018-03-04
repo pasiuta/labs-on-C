40463eeed3b8d92e9b70418e0623df29be047d84
dima <pasuta.dima@gmail.com>
2018-03-04 21:25:38 +0200

    added 1 laba

---
+++ lab_1/cmake-build-debug/CMakeFiles/lab_1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab_1.dir/main.c.o"
+  "CMakeFiles/lab_1.dir/dk_tool.c.o"
+  "lab_1.pdb"
+  "lab_1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lab_1/dk_tool.h
@@ -0,0 +1,15 @@
+//
+// Created by dima on 04.03.18.
+//
+
+#ifndef LAB_1_DK_TOOL_H
+#define LAB_1_DK_TOOL_H
+
+
+int Factorial (int j);
+
+float square(float a);
+
+float Factorialsum(float a);
+
+#endif //LAB_1_DK_TOOL_H
