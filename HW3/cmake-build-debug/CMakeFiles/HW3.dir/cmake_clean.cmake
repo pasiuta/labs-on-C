85cf5d5fe4f4d83544917fc66c8dfca2a2dd0918
dima <pasuta.dima@gmail.com>
2018-03-04 21:18:42 +0200

    added HW3 folder

---
+++ HW3/cmake-build-debug/CMakeFiles/HW3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW3.dir/main.c.o"
+  "HW3.pdb"
+  "HW3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HW3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
