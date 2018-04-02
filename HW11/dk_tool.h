9e0cf9f297fa9687ebb3b066bbdf62ba82fc296f
dima <pasuta.dima@gmail.com>
2018-04-02 20:44:31 +0300

    deleted 3 folders

--- HW11/dk_tool.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by dima on 10.03.18.
-//
-
-#ifndef HW1_DK_TOOL_H
-#define HW1_DK_TOOL_H
-void matrixsum (int length,  int firstmatrix[length][length], int secondmatrix[length][length]);
-#endif //HW1_DK_TOOL_H
